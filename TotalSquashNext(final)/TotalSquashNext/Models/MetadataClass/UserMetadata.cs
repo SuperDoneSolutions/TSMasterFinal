d54cb0dec3b7242b926e4879bfc2c34592a503ad
orangebandage <emily.e.bos@gmail.com>
2015-04-16 10:34:57 -0400

    Models changed
    Created a metadata class this time, because that's a good idea.

--- TotalSquashNext(final)/TotalSquashNext/Models/MetadataClass/UserMetadata.cs
+++ TotalSquashNext(final)/TotalSquashNext/Models/MetadataClass/UserMetadata.cs
@@ -4,9 +4,15 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace TotalSquashNext.Models.MetadataClasses
+namespace TotalSquashNext.Models
 {
-    public class UserMetadata
+    [MetadataType(typeof(UserMetadata))]
+
+    public partial class User
+    {
+    
+    }
+    public class UserMetaData
     {
         [Display(Name = "User ID")]
         public int id { get; set; }
@@ -14,22 +20,17 @@ namespace TotalSquashNext.Models.MetadataClasses
         [Display(Name = "Username")]
         public string username { get; set; }
 
-        [Display(Name = "Skill Level")]
+        [Display(Name = "Skill ID")]
         public int skillId { get; set; }
 
         [Display(Name = "Password")]
         public string password { get; set; }
 
-        [Display(Name = "Photo")]
+        [Display(Name = "Profile Photo")]
         public string photo { get; set; }
 
-        [Display(Name = "Wins")]
         public Nullable<int> wins { get; set; }
-
-        [Display(Name = "Losses")]
         public Nullable<int> losses { get; set; }
-
-        [Display(Name = "Ties")]
         public Nullable<int> ties { get; set; }
 
         [Display(Name = "First Name")]
@@ -38,7 +39,7 @@ namespace TotalSquashNext.Models.MetadataClasses
         [Display(Name = "Last Name")]
         public string lastName { get; set; }
 
-        [Display(Name = "Address")]
+        [Display(Name = "Street Address")]
         public string streetAddress { get; set; }
 
         [Display(Name = "City")]
@@ -53,16 +54,16 @@ namespace TotalSquashNext.Models.MetadataClasses
         [Display(Name = "Phone Number")]
         public string phoneNumber { get; set; }
 
-        [Display(Name = "E-mail")]
+        [Display(Name = "Email Address")]
         public string emailAddress { get; set; }
 
-        [Display(Name = "Identified Gender")]
+        [Display(Name = "Gender")]
         public string gender { get; set; }
 
-        [Display(Name = "Date of Birth")]
+        [Display(Name = "Birth Date")]
         public System.DateTime birthDate { get; set; }
 
-        [Display(Name = "Account Type")]
+        [Display(Name = "Account ID")]
         public int accountId { get; set; }
 
         [Display(Name = "Locked")]
@@ -72,6 +73,10 @@ namespace TotalSquashNext.Models.MetadataClasses
         public int organizationId { get; set; }
 
         [Display(Name = "Postal Code")]
+        [RegularExpression(@"[A-Za-z]\d[A-Za-z] ?\d[A-Za-z]\d", ErrorMessage = "Please enter like: L7N2Y2")]
         public string postalCode { get; set; }
+
+        [Display(Name = "Strike")]
+        public Nullable<int> strike { get; set; }
     }
 }
